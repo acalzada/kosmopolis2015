30ebb3a7496a149ac76f78b12de0a215f6934f28
iliars <ivan@MacBook-Air-de-Ivan.local>
2015-01-24 18:11:36 +0100

    Recuperar enllaços de google

---
+++ search.py
@@ -0,0 +1,46 @@
+import urllib2
+
+def getgoogleurl(search,siteurl=False):
+    if siteurl==False:
+        return 'http://www.google.com/search?q='+urllib2.quote(search)
+    else:
+        return 'http://www.google.com/search?q=site:'+urllib2.quote(siteurl)+'%20'+urllib2.quote(search)
+
+def getgooglelinks(search,siteurl=False):
+   #google returns 403 without user agent
+   headers = {'User-agent':'Mozilla/11.0'}
+   req = urllib2.Request(getgoogleurl(search,siteurl),None,headers)
+   site = urllib2.urlopen(req)
+   data = site.read()
+   site.close()
+
+   #no beatifulsoup because google html is generated with javascript
+   start = data.find('<div id="res">')
+   end = data.find('<div id="foot">')
+   if data[start:end]=='':
+      #error, no links to find
+      return False
+   else:
+      links =[]
+      data = data[start:end]
+      start = 0
+      end = 0        
+      while start>-1 and end>-1:
+          #get only results of the provided site
+          if siteurl==False:
+            start = data.find('<a href="/url?q=')
+          else:
+            start = data.find('<a href="/url?q='+str(siteurl))
+          data = data[start+len('<a href="/url?q='):]
+          end = data.find('&amp;sa=U&amp;ei=')
+          if start>-1 and end>-1: 
+              link =  urllib2.unquote(data[0:end])
+              data = data[end:len(data)]
+              if link.find('http')==0:
+                  links.append(link)
+      return links
+
+#links = getgooglelinks('python','http://www.stackoverflow.com/')
+links = getgooglelinks('artur+mas')
+for link in links:
+       print link
