53141f0573d478e4734a19c9bf2dee1c4c183ea1
Angel <merdaalcanto@gmail.com>
2015-01-24 16:11:08 +0100

    added functionality for removing punctuation marks and capital letters.

--- countWords.py
+++ countWords.py
@@ -5,6 +5,7 @@ This script counts the words in the input file and returns a dictionary
 with the word count for each word found in the file.
 
 """
+import string;
 def countWords(text):
 
 	wordDict = dict();
@@ -12,6 +13,10 @@ def countWords(text):
 	#	words = line.split();
 	words = text.split();
 	for word in words:
+		word = word.lower();
+		exclude = set(string.punctuation)
+		word = ''.join(ch for ch in word if ch not in exclude)
+		#word = word.replace(',', '');
 		wordDict[word] = wordDict.get(word,0)+1;
 
 	wordCount = [ (v,k) for k,v in wordDict.iteritems() ]
