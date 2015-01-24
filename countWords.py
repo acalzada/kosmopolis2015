a1219364f04b6e92fb5758320e7b5994a244140b
Angel <merdaalcanto@gmail.com>
2015-01-24 15:25:42 +0100

    Make file countWords.py into function

--- countWords.py
+++ countWords.py
@@ -5,24 +5,15 @@ This script counts the words in the input file and returns a dictionary
 with the word count for each word found in the file.
 
 """
+def countWords(text):
 
-name = raw_input("Enter file:")
-if len(name) < 1 : name = "textFile.txt"
-
-handle = open(name)
-
-wordDict = dict();
-for line in handle:
-	words = line.split();
+	wordDict = dict();
+	#for line in text:
+	#	words = line.split();
+	words = text.split();
 	for word in words:
 		wordDict[word] = wordDict.get(word,0)+1;
 
-#sortedDict = sorted(wordDict,key=itemgetter(1));
-#for data in sortedDict:
-#	print data
-
-word_view = [ (v,k) for k,v in wordDict.iteritems() ]
-word_view.sort(reverse=True) # natively sort tuples by first element
-#for v,k in word_view:
-#	    print "%s: %d" % (k,v)
-print word_view
+	wordCount = [ (v,k) for k,v in wordDict.iteritems() ]
+	wordCount.sort(reverse=True) # natively sort tuples by first element
+	return wordCount
